7bf27ed96463fa944c778b8243599fe0092e4ba9
Amelia Morton <an.morton16@gmail.com>
2019-01-13 18:02:43 -0500

    Dice Objects

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,21 @@
+const d20 {
+    pool: 0
+    sides: 20
+    roll: () => {
+        return Math.floor(Math.random()*20) + 1;
+    }
+}
+
+const d6 {
+    pool: 0
+    sides: 6
+    roll: () => {
+        return Math.floor(Math.random()*6) + 1;
+    }
+}
+
 $(function() {
-    //stuff
+    const addDie = dieType => {
+        
+    }
 });
